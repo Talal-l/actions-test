1360e1488f42f8107df6a0c1cbdee34e95996c41
T__T <t__t@pop-os.localdomain>
2021-11-28 18:27:52 +0300

    i

--- sync.sh
+++ sync.sh
@@ -1,4 +1,11 @@
 echo "input: $INPUT_WHO_TO_GREET"
 name="sub-test"
-url="git@github.com:Talal-l/sub-test.git"
-git subtree push --prefix=$name $url main
+#url="git@github.com:Talal-l/sub-test.git"
+url="git@p.github.com:Talal-l/sub-test.git"
+
+git subtree split --prefix $name -b $name # create a local  branch containing the splitted output folder
+git push  $url $name:$name # force the push of the gh-pages branch to the remote gh-pages branch at origin
+git branch -D $name # delete the local gh-pages because you will need it: ref
+
+
+#git subtree push --prefix=$name $url main
