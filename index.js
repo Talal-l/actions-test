4d783c8cba970729dcf5c36390abbf1025ba6453
T__T <t__t@pop-os.localdomain>
2021-11-28 17:03:45 +0300

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const { execSync } = require("child_process");
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -10,6 +11,9 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+
+  let gitOut = execSync("git status");
+  console.log("git", gitOut.toString());
 } catch (error) {
   core.setFailed(error.message);
 }
